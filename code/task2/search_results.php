f5ef28672bcc48b29aa73d73dd6c38577791c50d
turuslan <turuslan.devbox@gmail.com>
2015-09-11 11:36:31 +0300

    task2+, improved search - search by keywords rather than by plain string

--- code/task2/search_results.php
+++ code/task2/search_results.php
@@ -58,30 +58,51 @@ function sql_query ($q) {
 		// build sql query
 		// uses LOWER (...) and search query string in lowercase in order to perform case independent search
 		// uses LIKE '%q%' to determine if 'q' is in column
-		$query = "SELECT * FROM Publications WHERE LOWER (Title) LIKE '%$q%' OR LOWER (Author) LIKE '%$q%';";
+		$query = "SELECT * FROM Publications";
+		$qws = array ();
+		// if search query string is empty search anything
+		// otherwise add conditions
+		if (0 < strlen ($q)) {
+			$query .= " WHERE ";
+			// split search query string to keywords
+			$qs = explode (" ", $q);
+			// remove duplicates
+			$qs = array_unique ($qs);
+			for ($i = 0; $i < count ($qs); $i ++) {
+				$qw = $qs[$i];
+				$qws[] = $qw;
+				$qs[$i] = "LOWER (Title) LIKE '%$qw%' OR LOWER (Author) LIKE '%$qw%'";
+			}
+			$query .= implode (" OR ", $qs);
+		}
+		$query .= ";";
 
 		$rows = false;
-		// try to perform queru
+		// try to perform query
 		if ($rs = pg_query ($con, $query)) {
 			$rows = array ();
-			$q_len = strlen ($q);
 			// parse rows one by one
 			while ($row = pg_fetch_row ($rs)) {
 				// highlight occurences
-				if ($highlight) {
-					for ($j = 1; $j < 3; $j ++) {
-						$src = $row[$j];
-						$low = strtolower ($src);
-						$new = "";
-						$p = 0;
-						$op = $p;
-						while (($p = strpos ($low, $q, $op)) !== false) {
-							$new .= substr ($src, $op, $p - $op)."<mark>".html (substr ($src, $p, $q_len))."</mark>";
-							$p += $q_len;
+				if ($highlight && 0 < strlen ($q)) {
+					// for each keyword
+					for ($k = 0; $k < count ($qws); $k ++) {
+						$qw = $qws[$k];
+						$qw_len = strlen ($qw);
+						for ($j = 1; $j < 3; $j ++) {
+							$src = $row[$j];
+							$low = strtolower ($src);
+							$new = "";
+							$p = 0;
 							$op = $p;
+							while (($p = strpos ($low, $qw, $op)) !== false) {
+								$new .= substr ($src, $op, $p - $op)."<mark>".html (substr ($src, $p, $qw_len))."</mark>";
+								$p += $qw_len;
+								$op = $p;
+							}
+							$new .= substr ($src, $op, strlen ($src) - $op);
+							$row[$j] = $new;
 						}
-						$new .= substr ($src, $op, strlen ($src) - $op);
-						$row[$j] = $new;
 					}
 				}
 				// add `row` to `rows` array
@@ -105,6 +126,12 @@ if (!isset ($_GET["q"])) {
 }
 // make search query string lowercase
 $get_q = strtolower ($_GET["q"]);
+
+// remove whitespace duplicates
+$get_q = preg_replace ("/\W+/", " ", $get_q);
+// remove whitespace at the begin and the end
+$get_q = trim ($get_q);
+
 // do the query
 $rows = sql_query ($get_q);
 
