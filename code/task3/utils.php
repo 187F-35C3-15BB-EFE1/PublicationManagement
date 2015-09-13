573492a4fc13b53c95a6033ec6aea2e377b5d296
turuslan <turuslan.devbox@gmail.com>
2015-09-13 17:55:33 +0300

    task3, utils.php: redirect, working with html templates

---
+++ code/task3/utils.php
@@ -0,0 +1,24 @@
+<?php
+function utils_redirect ($to) {
+	header ("Location: $to");
+}
+function html ($s) {
+	return htmlspecialchars ($s);
+}
+function page_load ($p) {
+	return $page = file_get_contents ($p);
+}
+function page_replace ($p, $m, $x) {
+	return str_replace ($m, $x, $p);
+}
+function page_split ($p, $ml, $mr) {
+	$il = strpos ($p, $ml);
+	$ll = strlen ($ml);
+	$ir = strpos ($p, $mr, $il + $ll);
+	$lr = strlen ($mr);
+	$b = substr ($p, $il);
+	$c = substr ($p, $il + $ll, $ir);
+	$a = substr ($p, $ir + $lr, strlen ($p));
+	return array (b, c, a);
+}
+?>
