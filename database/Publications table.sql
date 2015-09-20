f613795b684cfadbce6d79fa1e2cd131ea8facd8
EiffelBoy <dantist1990@gmail.com>
2015-09-20 17:35:29 +0300

    Added database folder; ER model, and .sql file where database and publications table are set up

---
+++ database/Publications table.sql
@@ -0,0 +1,15 @@
+--creation of database
+CREATE DATABASE pms;
+
+--to go into database type \connect pms
+--now we create table publications
+CREATE TABLE publications (
+pid SERIAL PRIMARY KEY,
+title VARCHAR(255) NOT NULL,
+authors VARCHAR(255) NOT NULL DEFAULT 'Anonymous',
+research_field VARCHAR(255) NOT NULL,
+publication_year INTEGER NOT NULL,
+venue VARCHAR(255) NOT NULL,
+papertype VARCHAR(255) NOT NULL);		--????
+
+--That is all for now kids!
